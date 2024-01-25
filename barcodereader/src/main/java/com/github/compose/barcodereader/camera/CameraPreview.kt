51a780b18b8b5ded1e991895d8445cde83be8638
mokaab.dev <mohamadreza.shahpiri@gmail.com>
2024-01-26 00:10:36 +0330

    mlkit and zxing added

--- barcodereader/src/main/java/com/github/compose/barcodereader/camera/CameraPreview.kt
+++ barcodereader/src/main/java/com/github/compose/barcodereader/camera/CameraPreview.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
